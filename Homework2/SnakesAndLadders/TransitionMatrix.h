c3a60dee6ff9a9c09b24f45e1d2bb48334f4ca59
staticninja <staticninja23@gmail.com>
2017-03-16 11:24:48 -0400

    Restart Queue Folder

--- Homework2/SnakesAndLadders/TransitionMatrix.h
+++ Homework2/SnakesAndLadders/TransitionMatrix.h
@@ -1,10 +1,10 @@
-int size = 0;  //TODO
+int size = 101;  //TODO
 Eigen::MatrixXf TransitionMatrix(size, size);
 Eigen::VectorXf v(size);
 
 unsigned int ROLLS = 0; //TODO
 
-double prob = 0.0 ;  //TODO
+double prob = 1.0/6.0;  //TODO
 
 
 
@@ -12,7 +12,13 @@ double prob = 0.0 ;  //TODO
 void SetTransitionMatrix()
 {
 	TransitionMatrix.setZero();
+	for (int i = 1; i < size; i++)
+	{
+		for (int j = 1; j < size; j++)
+		{
 
+		}
+	}
 	//TODO
 
 
